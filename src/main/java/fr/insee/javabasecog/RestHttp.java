1fec1997926feed0750c69dcd957d80dcbfea0b7
Fabrice Bibonne <59668259+FBibonne@users.noreply.github.com>
2022-06-30 08:41:18 +0200

    Entretien jeudi

--- src/main/java/fr/insee/javabasecog/RestHttp.java
+++ src/main/java/fr/insee/javabasecog/RestHttp.java
@@ -1,25 +1,13 @@
 package fr.insee.javabasecog;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ProxySelector;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
+
 
 public class RestHttp {
 
 
-    public String get(URI uri, String bearer) throws IOException, InterruptedException {
-        var client = HttpClient.newBuilder().build();
-        var httpRequest = HttpRequest.newBuilder().header("Accept", "application/json")
-                .header("Authorization", "Bearer "+bearer)
-                .GET()
-                .uri(uri)
-                .build();
-        return client.send(httpRequest, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8)).body();
+    public String get(URI uri, String bearer) throws IOException {
+
     }
 
 }
